cdbf015082441e17881e37a34f2b4785e2e150a7
qaismon <qaismon127@gmail.com>
2026-01-30 10:55:01 +0530

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
+const cors = require('cors');
+
+app.use(cors({
+  origin: "https://gainlabz.onrender.com/" // Your Static Site URL
+}));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
